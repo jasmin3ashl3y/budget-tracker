e68abf750e36b3c325922682a1b4f48df9d4b301
jasmin3ashl3y <jasmine@ashleyit.com>
2022-01-15 21:55:36 -0500

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,7 +7,7 @@ const FILES_TO_CACHE = [
     "/styles.css",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png",
-    "./manifest.json",
+    "../manifest.json",
     "/idb.js",
     "/index.js",
 ]
